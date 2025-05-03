4b0b21f3af3c5f350bce2736a59ef1538a284ba5
Kevin <kevinbatchelor@gmail.com>
2025-05-02 21:52:08 -0600

    fix db update

--- server/util/database/dbSetup.js
+++ server/util/database/dbSetup.js
@@ -1,5 +1,7 @@
 const db = require('./db');
 const Category = require('../../movie/movieCategory');
+const Movie = require('../../movie/movie');
+const Audiobook = require('../../audioBooks/audioBook');
 
 const setup = function () {
     console.log('set up database')
